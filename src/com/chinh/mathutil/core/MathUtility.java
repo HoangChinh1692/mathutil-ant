e5f1ed91cdba7dadd3e0d45c1f60c2a984046da2
HoangChinh1692 <chinhnguyen1692000z@gmail.com>
2023-02-21 08:52:58 +0700

    update getfatorial() too see green again

--- src/com/chinh/mathutil/core/MathUtility.java
+++ src/com/chinh/mathutil/core/MathUtility.java
@@ -31,9 +31,9 @@ public class MathUtility {
         //sống sót đến đây, sure n =2..20
         //for hay đệ quy mà 
         long product = 1; //biến tích lũy, gom việc nhân i vào
-        for (int i = 2; i < n; i++) { // make code wrong
-            product *= i;
-        }
+        for (int i = 2; i <= n; i++) { // do not make code wrong
+            product *= i;               // if upload this modification
+        }                               // we will see the GREEN again!!!
 
         return product;
     }
